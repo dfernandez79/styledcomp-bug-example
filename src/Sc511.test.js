95287183a7f1b280a530927ee191502580dd15ee
Diego Fernandez <dfernandez@asapp.com>
2020-09-07 10:17:59 -0300

    Changes to show the bug

---
+++ src/Sc511.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import styled from "styled-components511";
+
+const TestComponent = styled.div`
+  background-color: #0000ff;
+`;
+
+test("Get computed style", () => {
+  const testComponent = render(
+    <TestComponent data-testid="test" />
+  ).getByTestId("test");
+
+  expect(window.getComputedStyle(testComponent).backgroundColor).toBe(
+    "rgb(0, 0, 255)"
+  );
+});
